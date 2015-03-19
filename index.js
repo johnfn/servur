a165f38a1a69fbe060ed94b91271565e8ca9af45
johnfn <johnfn@gmail.com>
2015-03-18 17:39:04 -0700

    Add psql

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -7,6 +8,21 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('select * from json_table', function(err, result) {
+      done();
+
+      if (err) {
+        console.error(err);
+        response.send("Error " + err);
+      } else {
+        response.send(result.rows);
+      }
+    });
+  });
+})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
