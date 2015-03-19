48340f360eba0f9365646cf35d1fd101441534ce
johnfn <johnfn@gmail.com>
2015-03-18 22:39:11 -0700

    Upsert changes into the db

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
 var pg = require('pg');
+var bodyParser = require('body-parser')
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
@@ -23,6 +26,25 @@ app.get('/db', function (request, response) {
     });
   });
 })
+
+app.post('/save', function(req, res) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    var upsert  = "UPDATE json_table SET json='" + JSON.stringify(req.body) + "' WHERE id=1;" +
+                  "INSERT INTO json_table (id, json)" +
+                  "SELECT 1, '" + JSON.stringify(req.body) + "'" +
+                  "WHERE NOT EXISTS (SELECT 1 FROM json_table WHERE id = 1);";
+
+    client.query(upsert, function(err, result) {
+      if (err) {
+        console.log(err);
+      }
+
+      done();
+
+      res.send("lol");
+    });
+  });
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
